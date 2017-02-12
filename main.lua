5e423c246b677cca7e493cd2b40fda7967692f7b
Andrew Lilja <andrewlilja@gmail.com>
2017-02-11 20:44:13 -0600

    added hang time and animations i am bad at git

--- main.lua
+++ main.lua
@@ -1,21 +1,51 @@
-function love.load()
-	inspect = require('lib.inspect')
+local inspect = require 'lib.inspect'
+local anim8 = require 'lib.anim8'
 
+local image, animation
+
+function love.load()
 	love.window.setMode(1216, 760, {})
+	love.graphics.setBackgroundColor(89, 86, 82)
+	love.graphics.setDefaultFilter("nearest")
+
+
+	idle_image = love.graphics.newImage('images/an-1x/idle.png')
+	local g = anim8.newGrid(32, 32, idle_image:getWidth(), idle_image:getHeight())
+	idle_animation = anim8.newAnimation(g('1-5',1), 0.1)
+
+	-- do the same for left and right facing
+
+	right_run_image = love.graphics.newImage('images/an-1x/run.png')
+	local g = anim8.newGrid(32, 32, right_run_image:getWidth(), right_run_image:getHeight())
+	right_run_animation = anim8.newAnimation(g('1-8',1), 0.1):flipH()
+
+	left_run_image = love.graphics.newImage('images/an-1x/run.png')
+	local g = anim8.newGrid(32, 32, left_run_image:getWidth(), left_run_image:getHeight())
+	left_run_animation = anim8.newAnimation(g('1-8',1), 0.1)
+
+
+	animation = idle_animation
+	image = idle_image
+
+
+
+
+
+
 
 	player = {}
 	player.x = 100
 	player.y = 300
-	player.height = 30
-	player.width = 20
+	player.height = 64
+	player.width = 64
 
 	vel = 0
 	decay = 0.74
 	acceleration = 80 -- pixels per second
-	max_velocity = 700
+	max_velocity = 400
 
-	jump_force = -1050
-	jump_cutoff = -300 -- if your speed is below this, you are fixed to this speed
+	jump_force = -1250
+	jump_cutoff = -400 -- if your speed is below this, you are fixed to this speed
 					   -- basically, the lower this number, the sooner you are fixed
 					   -- to a low jump
 	jump_tolerance = 20 -- how far away from the ground can you be before you can
@@ -25,7 +55,9 @@ function love.load()
 	reactivity_percent = 1.95 -- how quickly you start moving in the opposite direction
 
 	jump_vel = 0
-	gravity = 80
+	gravity = 60
+	hang_time = 0
+	max_hang_time = 0.5
 
 	is_jumping = false
 	jump_tolerance_trigger = false
@@ -34,13 +66,15 @@ function love.load()
 	air_accel_control = 2.2
 	air_vel_control = 0.9
 	air_reactivity = 0.6
-	air_decay = 0.97
+	air_decay = 0.94
 
 	combos = {}
 	combo_time = 0
 	max_combo_time = 0.5
 
 	ground = love.graphics.getHeight() - 150
+
+	facing = "left"
 end
 
 function calculate_horizontal_speed(direction, velocity)
@@ -89,7 +123,6 @@ function love.keypressed(key)
 		print("s pressed")
 		table.insert(combos, "s")
 	end
-
 end
 
 function love.keyreleased(key)
@@ -101,10 +134,14 @@ function love.keyreleased(key)
 end
 
 function check_combos(combos)
-	if #combos == 2 then
+	if #combos == 4 then
 		if combos[1] == "a" then
-			if combos[2] == "a" then
-				return "double a"
+			if combos[2] == "s" then
+				if combos[3] == "a" then
+					if combos[4] == "s" then
+						return "asas"
+					end
+				end
 			end
 		end
 	elseif #combos == 3 then
@@ -115,14 +152,10 @@ function check_combos(combos)
 				end
 			end
 		end
-	elseif #combos == 4 then
+	elseif #combos == 2 then
 		if combos[1] == "a" then
-			if combos[2] == "s" then
-				if combos[3] == "a" then
-					if combos[4] == "s" then
-						return "asas"
-					end
-				end
+			if combos[2] == "a" then
+				return "double a"
 			end
 		end
 	end
@@ -131,16 +164,29 @@ end
 
 
 function love.update(dt)
+
 	if love.keyboard.isDown("right") then
 		vel = calculate_horizontal_speed("right", vel)
+
+		animation = right_run_animation
+		image = right_run_image
 	elseif love.keyboard.isDown("left") then
 		vel = calculate_horizontal_speed("left", vel)
+
+		animation = left_run_animation
+		image = left_run_image
 	else
 		if is_jumping then
 			vel = vel * air_decay
 		else
 			vel = vel * decay
 		end
+	end
+
+
+	if math.abs(vel) < 0.1 then
+		animation = idle_animation
+		image = idle_image
 	end
 
 	if next(combos) ~= nil then
@@ -155,7 +201,7 @@ function love.update(dt)
 			if combo == "double a" then
 				--impulse = 10000
 				--if is_jumping then impulse = impulse * 2 end
-
+				max_hang_time = 0.1
 				distance = 300
 				if is_jumping then distance = distance * 1.5 end
 				if love.keyboard.isDown("right") then
@@ -169,7 +215,7 @@ function love.update(dt)
 				if love.keyboard.isDown("up") then
 					--jump_vel = jump_vel - impulse/15
 					player.y = player.y - distance/2
-					jump_vel = jump_vel + jump_force
+					max_hang_time = 0.3
 				end
 				if love.keyboard.isDown("down") then
 					--jump_vel = jump_vel - impulse/15
@@ -187,6 +233,16 @@ function love.update(dt)
 
 	jump_vel = jump_vel + gravity
 	if jump_vel > 500 then jump_vel = 500 end
+	if jump_vel > 0 then
+		-- we're falling, change animations as needed
+	end
+	if hang_time > max_hang_time then
+		hang_time = 0
+		max_hang_time = 0
+	elseif max_hang_time > 0 then
+		jump_vel = 0
+		hang_time = hang_time + dt
+	end
 
 	player.x = player.x + vel * dt
 	player.y = player.y + jump_vel * dt
@@ -202,9 +258,11 @@ function love.update(dt)
 
 	if player.x > love.graphics.getWidth() then player.x = 0 end
 	if player.x < 0 then player.x = love.graphics.getWidth() end
+
+	animation:update(dt)
 end
 
 function love.draw()
-	love.graphics.rectangle("line", player.x, player.y, player.width, player.height)
+	animation:draw(image, player.x, player.y, 0, 2, 2)
 	love.graphics.line(0, ground + player.height + 1, love.graphics.getWidth(), ground + player.height + 1)
 end
